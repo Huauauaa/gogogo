10046f94f671aa92f11ee85b7f04eb0fe09721a9
huaweiwei <harvey0379@163.com>
2022-02-16 22:29:19 +0800

    ✨ array

--- README.md
+++ README.md
@@ -8,3 +8,17 @@
 
 1. `go build test/hello.go` `./hello.exe`/`./hello`
 2. `go run test/hello.go`
+
+## 数组
+
+### 定长数组
+
+```go
+var a = [3]int
+```
+
+### 动态数组
+
+```go
+a := []int{1,2,3}
+```
---
+++ test/array.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myArray [10]int
+
+	myArray2 := [10]int{1, 2, 3}
+
+	fmt.Println(myArray, myArray2)
+	fmt.Printf("myArray2 type is %T\n", myArray2)
+
+	for i := 0; i < len(myArray); i++ {
+		fmt.Println(myArray[i])
+	}
+
+	for index, value := range myArray2 {
+		fmt.Println("index =", index, ", value =", value)
+	}
+
+	myArray3 := []int{1, 3, 5}
+
+	for i := 0; i < len(myArray3); i++ {
+		fmt.Println(myArray3[i])
+	}
+	fmt.Printf("myArray3 type is %T\n", myArray3)
+	for index, value := range myArray3 {
+		fmt.Println(index, value)
+	}
+}
